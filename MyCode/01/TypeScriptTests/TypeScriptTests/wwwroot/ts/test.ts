1ea7128a3b6c237dd7dd0cb6ebfe6406d8dad457
Wagner Xu <wagnerhsu@hotmail.com>
2020-08-07 23:16:16 +0800

    Add files

---
+++ MyCode/01/TypeScriptTests/TypeScriptTests/wwwroot/ts/test.ts
@@ -0,0 +1,6 @@
+﻿var firstName: string = "Francesco";
+var surName: string = "Abbruzzese";
+function fullName(x: string, y: string, spaces: number): string {
+    return x + Array(spaces + 1).join(' ') + y;
+}
+alert(fullName(firstName, surName, 3) + " Hello");
